a9fcb6f744c7f0ba060766207ed07849d68cd08e
MDEWD <1539570130@qq.com>
2024-06-30 19:36:46 +0800

    update interfaceInfo

---
+++ src/main/java/com/api/project/mapper/InterfaceInfoMapper.java
@@ -0,0 +1,15 @@
+package com.api.project.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.api.project.model.entity.InterfaceInfo;
+
+/**
+* @author Dan Wang
+* @description 针对表【interface_info(接口信息)】的数据库操作Mapper
+* @createDate 2024-06-21 09:50:10
+* @Entity com.api.project.model.entity.InterfaceInfo
+*/
+public interface InterfaceInfoMapper extends BaseMapper<InterfaceInfo> {
+
+
+}
